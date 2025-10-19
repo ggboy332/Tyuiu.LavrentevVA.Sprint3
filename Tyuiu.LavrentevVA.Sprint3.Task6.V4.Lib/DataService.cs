6ba964f54e345d18ac838376b67581e0f38ade2e
dimka <dimka556.dim4@gmail.com>
2025-10-19 20:53:47 +0500

    Task6

---
+++ Tyuiu.LavrentevVA.Sprint3.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3; 
+namespace Tyuiu.LavrentevVA.Sprint3.Task6.V4.Lib
+{
+    public class DataService : ISprint3Task6V4
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int x;
+            int sum = 0;
+            for (x = startValue; x <= stopValue; x++)
+            {
+                for (int i = 9; i <= x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint3.Task6.V4/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.LavrentevVA.Sprint3.Task6.V4.Lib;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Tyuiu.LavrentevVA.Sprint3.Task6.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                                                                                                                                            *");
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+
+            int startValue1 = 14;
+            int stopValue1 = 20;
+
+            Console.WriteLine($"начало отрезка = {startValue1}");
+            Console.WriteLine($"конец отрезка = {stopValue1}");
+
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+            Console.WriteLine($"Сумма всех делителей больше 9 на отрезке: {ds.GetSumTheDivisors(startValue1, stopValue1)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
